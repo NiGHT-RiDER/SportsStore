193b07ffacd66e01788fd832c4a6e095c5656df8
NiGHT-RiDER <NiGHTRiDERgithub@tfwno.gf>
2016-10-14 15:38:47 +0200

    Il reste a faire l'implementation de IOrdreProcessor

---
+++ Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EfDbContext context = new EfDbContext();
+
+        public IEnumerable<Product> Products
+        {
+            get { return context.Products; }
+        }
+
+        public Product deleteProduct(int productID)
+        {
+            Product toDelete = context.Products.Find(productID);
+            if(toDelete != null)
+            {
+                context.Products.Remove(toDelete);
+                context.SaveChanges();
+            }
+            return toDelete;
+        }
+
+
+        public void SaveProduct(Product product)
+        {
+            if(product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product entreeDB = context.Products.Find(product.ProductID);
+                if(entreeDB != null)
+                {
+                    entreeDB.Name = product.Name;
+                    entreeDB.Description = product.Description ;
+                    entreeDB.Price= product.Price;
+                    entreeDB.Category= product.Category;
+                    entreeDB.ImageData= product.ImageData;
+                    entreeDB.ImageMimeType= product.ImageMimeType;
+                }
+            }
+            context.SaveChanges();
+        }
+    }
+}
